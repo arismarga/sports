2b6c96291b302c4d7328daab6f4ae3a83fbc2c0a
arismarga <aris.margaritopoulos@gmail.com>
2022-07-23 20:46:17 +0300

    Fetch Games on initial load

--- src/js/sports.js
+++ src/js/sports.js
@@ -6,6 +6,24 @@ class Teams {
     constructor() {
         this.fetchTeams();
     }
+
+    fetchTeams(){
+        const options = {
+            method: 'GET',
+            url: 'https://free-nba.p.rapidapi.com/teams',
+            params: {page: '0'},
+            headers: {
+              'X-RapidAPI-Key': '54e8d1a6f4mshe9f62bb56442508p1ab909jsne54dd6094676',
+              'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then((response) => {
+            this.createTeamOptions(response);
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
 }
 
 new Teams();
